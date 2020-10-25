ec929887e286e95cea19dd72580976afa25295bb
juanmaureira <juanmaureira3@gmail.com>
2020-10-25 01:34:58 -0300

    Aplicacion funcionando con Boton para convertir

--- app/src/main/java/cl/maureira/appdivisas/MainActivity.java
+++ app/src/main/java/cl/maureira/appdivisas/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,32 +29,48 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RequestQueue queue;
-    private TextView text;
-    String urlDivisas = "https://free.currconv.com/api/v7/convert?q=USD_EUR&compact=ultra&apiKey=51f15bfbcb6f5d386ff0";
+    public EditText valor;
+    public EditText resultado;
+    public String divisaIn;
+    public String divisaOut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = findViewById(R.id.tvMonedas);
+
+        valor = findViewById(R.id.etImporte);
+        resultado = findViewById(R.id.etResultado);
+
         queue = Volley.newRequestQueue(this);
-        getDivisas();
     }
 
     public void getConversion(View view){
+        Spinner spDivisaIn = (Spinner) findViewById(R.id.spMoneda1);
+        Spinner spDivisaOut = (Spinner) findViewById(R.id.spMoneda2);
 
+        divisaIn = spDivisaIn.getSelectedItem().toString();
+        divisaOut = spDivisaOut.getSelectedItem().toString();
+
+        String conversor = divisaIn + "_" + divisaOut;
+        Log.d("divisaIn", divisaIn);
+        String urlDivisas = "https://free.currconv.com/api/v7/convert?q=" + conversor + "&compact=ultra&apiKey=51f15bfbcb6f5d386ff0";
+        Log.d("url", urlDivisas);
+        getDivisas(urlDivisas, conversor);
     }
 
-    private void getDivisas() {
+    private void getDivisas(String url, String conversion) {
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urlDivisas,null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url,null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    String data = response.getString("USD_EUR");
+                    String data = response.getString(conversion);
                     Log.d("Lista", data);
-                    //                    JSONObject data = response.getJSONObject("results");
-//                    Log.d("Lista", String.valueOf(data));
+                    Float factor = Float.parseFloat(data);
+                    Float res = Float.valueOf(factor * Float.parseFloat(valor.getText().toString()));
+                    resultado.setText(res.toString());
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -60,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
         }, new Response.ErrorListener(){
             @Override
             public void onErrorResponse(VolleyError error) {
-                text.setText(error.toString());
                 Toast.makeText(MainActivity.this, "ERROR" + error, Toast.LENGTH_SHORT).show();
             }
         });
